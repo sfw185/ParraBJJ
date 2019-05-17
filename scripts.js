ead0c4a43851a25c3bb1aff66a7ada0a3a9ea518
Simon Wardan <simon@wardan.com>
2019-05-17 15:57:05 +1000

    Refactoring and WIP on fetching weekly schedule

---
+++ scripts.js
@@ -0,0 +1,76 @@
+var dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+function getStartTime() {
+  var start = new Date();
+  start.setHours(0,0,0,0);
+  return start;
+}
+
+function getEndTime() {
+  var end = getStartTime()
+  end.setDate(end.getDate() + 7)
+  return end;
+}
+
+function formatDate(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ampm;
+  return strTime;
+}
+
+function groupByStartDay(schedule) {
+  return schedule.reduce(function(aggregate, current) {
+    var dateBits = new Date(current["start"]).toLocaleDateString("en-AU", dateOptions).split(' ');
+    dateBits.pop();
+    var startDate = dateBits.join(' ');
+    console.log(startDate);
+    if (!aggregate[startDate]) { aggregate[startDate] = []; }
+    aggregate[startDate].push(current);
+    return aggregate;
+  }, {});
+}
+
+function getContainer() {
+  return document.getElementsByTagName('div')[0];
+}
+
+function processSchedule(schedule) {
+  var div = getContainer()
+  div.innerHTML = "";
+
+  var scheduleByDay = groupByStartDay(schedule)
+  var header = document.createElement('h1');
+  header.innerHTML = Object.keys(scheduleByDay)[0]
+  div.appendChild(header);
+
+  for (var x = 0; x < schedule.length; x++) {
+    var someClass = schedule[x];
+    var startTime = formatDate(new Date(someClass.start));
+    var p = document.createElement('p');
+    p.innerHTML = [
+      '<strong>' + startTime + '</strong>' + ' - ' + '<small>' + someClass.instructor_name.split(' ')[0] + '</small>',
+      '<strong><em>' + someClass.title.replace('-B', ' - B') + '</em></strong>'
+    ].join('<br/>');
+    div.appendChild(p);
+  }
+}
+
+function getSchedule() {
+  var div = getContainer();
+  div.innerHTML = "<p><strong>Loading...</strong></p>";
+
+  var request = new XMLHttpRequest();
+  var extra = "?start=" + String(Math.floor(getStartTime()/1000)) + "&end=" + String(Math.floor(getEndTime()/1000));
+  request.open("GET", "https://otbpujjwh8.execute-api.ap-southeast-2.amazonaws.com/default/gracieParra" + extra, true);
+  request.onreadystatechange = function () {
+    if (request.readyState != 4 || request.status != 200) return;
+    var schedule = JSON.parse(request.responseText)
+    processSchedule(schedule);
+  };
+  request.send();
+}
