7ea672c7a3046bc04765ab7d097b0be1503def4d
Simon Wardan <simon@wardan.com>
2019-10-17 13:57:05 +1100

    Added end date to classes

--- src/data.js
+++ src/data.js
@@ -10,7 +10,7 @@ const groupByStartDay = (schedule) =>
   schedule.reduce((aggregate, current) => {
     const startDate = new Date(current["start"]).toLocaleDateString("en-AU", { weekday: 'long' }).split(',')[0];
     if (!aggregate[startDate]) { aggregate[startDate] = []; }
-    fixDateFormat(current);
+    fixDates(current);
     aggregate[startDate].push(current);
     return aggregate;
   }, {});
@@ -22,29 +22,34 @@ const getScheduleData = async() => {
 
 const getS3BucketName = () => process.env.S3_BUCKET && (process.env.S3_BUCKET).length > 0 ? process.env.S3_BUCKET : null;
 
-const storeLocal = async (html) => writeFile('./public/index.html', html);
+const storeLocal = async (html, filename) => writeFile(`./public/${filename}`, html);
 
-const storeRemote = async (html) => await s3.putObject({
+const storeRemote = async (html, filename) => await s3.putObject({
   Body: Buffer.from(html, 'utf8'),
   ContentType: 'text/html',
   Bucket: getS3BucketName(),
-  Key: 'index.html'
+  Key: filename
 }).promise();
 
-const storeHTML = async (html) => {
-  console.log({ html });
-  return getS3BucketName() ? storeRemote(html) : storeLocal(html);
+const storeHTML = async (html, filename) => {
+  console.log(`Storing ${filename}`);
+  return getS3BucketName() ? storeRemote(html, filename) : storeLocal(html, filename);
 };
 
-const fixDateFormat = (someClass) => {
-  var date = new Date(someClass["start"]);
+const fixDateFormat = (raw_date) => {
+  const date = new Date(raw_date);
   var hours = date.getHours();
   var minutes = date.getMinutes();
   var ampm = hours >= 12 ? 'pm' : 'am';
   hours = hours % 12;
   hours = hours ? hours : 12;
   minutes = minutes < 10 ? '0'+minutes : minutes;
-  someClass.start = hours + ':' + minutes + ampm;
+  return hours + ':' + minutes + ampm;
+}
+
+const fixDates = (someClass) => {
+  someClass.start = fixDateFormat(someClass.start);
+  someClass.end = fixDateFormat(someClass.end);
 };
 
 module.exports = { getScheduleData, storeHTML };
